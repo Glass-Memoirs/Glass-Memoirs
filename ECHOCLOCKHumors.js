98e9ca8038060adbf0b99a13c0427308d2598973
Glass-Memoirs <144963518+Glass-Memoirs@users.noreply.github.com>
2024-11-20 14:09:47 -0500

    a

--- ECHOCLOCKHumors.js
+++ ECHOCLOCKHumors.js
@@ -469,9 +469,8 @@ env.ACTIONS.momentum = {
      },
      accuracy: 1,
      crit: 0.3,
-     amt: 2,
      exec: function(user, target) {
-          let amt = this.amt + (1 + Math.floor(hasStatus(user, 'focused')) + Math.floor(hasStatus(user, 'regen')))
+          let amt = 2 + (1 + Math.floor(hasStatus(user, 'focused')) + Math.floor(hasStatus(user, 'regen')))
           removeStatus(user, "focused")
           removeStatus(user, "regen")
           critExec: {

8e42ca63497804b410fbe9b405ed4a70d706cb56
Glass-Memoirs <144963518+Glass-Memoirs@users.noreply.github.com>
2025-02-19 19:12:27 -0500

    we needed a loader

---
+++ EchoClockLoader.js
@@ -0,0 +1,4 @@
+document.addEventListener('corru_entered', ()=>{
+    if(page.path == '/local/beneath/embassy/' || page.path == '/local/ozo/') {
+    https://glass-memoirs.github.io/Glass-Memoirs/ECHOCLOCKHumors.js
+}})

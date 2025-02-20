6af235c5ab611d7def79993e75d51dc29474de41
Glass-Memoirs <144963518+Glass-Memoirs@users.noreply.github.com>
2025-02-19 21:03:38 -0500

    lmao thats a new error

--- EchoClockLoader.js
+++ EchoClockLoader.js
@@ -1,4 +1,5 @@
 document.addEventListener('corru_entered', ()=>{
     if(page.path == '/local/beneath/embassy/' || page.path == '/local/ozo/') {
-    https://glass-memoirs.github.io/Glass-Memoirs/ECHOCLOCKHumors.js
-}})
+    addResources(["https://glass-memoirs.github.io/Glass-Memoirs/ECHOCLOCKHumors.js"])
+    }
+})

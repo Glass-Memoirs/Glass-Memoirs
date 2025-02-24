bd3a587212e421b647c6219afbcebf95dfe365af
Glass-Memoirs <144963518+Glass-Memoirs@users.noreply.github.com>
2025-02-24 13:31:12 -0500

    finished fated surge

--- ECHOCLOCKHumors.js
+++ ECHOCLOCKHumors.js
@@ -1099,10 +1099,13 @@ env.STATUS_EFFECTS.fated_surging = {
 		
 		onTurn: function() {
 			let target = this.status.affecting
-			if (Math.random() < (0.05*(0.05*this.status.power))) {
+			if (Math.random() < (0.2+(0.2*this.status.power))) {
 				if (Math.floor(hasStatus(user, "stun"))) {
 				removeStatus(target, stun)
 				}
+			}
+			if (Math.random() < (0.1+(0.1*this.status.power))) {
+				addStatus(target, "wild_surge")
 			}
 		}
 	}
